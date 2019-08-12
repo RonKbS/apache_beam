f1fba5f44ef33c5cc35f2f197ceceaae6f66c1e5
RonKbS <stephen.bamwite@gmail.com>
2019-08-12 17:56:36 +0300

    pip install apache-beam

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # apache_beam
+
+Apache Beam =>
+
+An open source, unified model for defining both batch and streaming data-parallel processing pipelines
+
+Using one of the open source Beam SDKs, a program is built that defines the pipeline. The pipeline is then executed by one of Beam’s supported distributed processing back-ends, which include Apache Apex, Apache Flink, Apache Spark, and Google Cloud Dataflow.
+
---
+++ notes.py
@@ -0,0 +1,10 @@
+
+
+# In parallel computing, an embarrassingly parallel workload or problem (also called perfectly parallel,
+# delightfully parallel or pleasingly parallel) is one where little or no effort is needed to separate
+# the problem into a number of parallel tasks.[1] This is often the case where there is little or no
+# dependency or need for communication between those parallel tasks, or for results between them.
+
+# Beam is particularly useful for Embarrassingly Parallel data processing tasks, in which the problem 
+# can be decomposed into many smaller bundles of data that can be processed independently and in parallel.
+# You can also use Beam for Extract, Transform, and Load (ETL) tasks and pure data integration.
